6493deccf40d31268410dd05db295ddc607932f7
Teimur Gasanov <teymurgg321@gmail.com>
2020-11-16 11:12:29 +0600

    Fix product update response

--- src/views/product/detail.py
+++ src/views/product/detail.py
@@ -60,7 +60,6 @@ class ProductDetailView(ValidatableView[UpdateProductData]):
             product = self._service.update(product_id, valid_data, user=request.user)
             serialized_product = (
                 self._serializer_cls(product)
-                .in_language(request.language)
                 .with_serialized_product_type()
                 .serialize()
             )
